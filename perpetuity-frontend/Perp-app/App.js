39d1755ec571392fa60da82cb76cf5c495414894
Lamont V <lxveal1983@gmail.com>
2021-05-09 12:18:57 -0400

    Add files via upload

---
+++ perpetuity-frontend/Perp-app/App.js
@@ -0,0 +1,112 @@
+import React, {useState} from 'react';
+import { KeyboardAvoidingView, StyleSheet, Text, View, TextInput, TouchableOpacity, Keyboard, ScrollView } from 'react-native';
+import Task from './components/Option';
+
+export default function App() {
+  const [option, setOption] = useState();
+  const [optionItems, setOptionItems] = useState([]);
+
+  const handleAddOption = () => {
+    Keyboard.dismiss();
+    setOptionItems([...optionItems, option])
+    setOption(null);
+  }
+
+  const completeOption = (index) => {
+    let itemsCopy = [...optionItems];
+    itemsCopy.splice(index, 1);
+    setOptionItems(itemsCopy)
+  }
+
+  return (
+    <View style={styles.container}>
+      {/* Added this scroll view to enable scrolling when list gets longer than the page */}
+      <ScrollView
+        contentContainerStyle={{
+          flexGrow: 1
+        }}
+        keyboardShouldPersistTaps='handled'
+      >
+
+      {/* Today's Tasks */}
+      <View style={styles.optionsWrapper}>
+        <Text style={styles.sectionTitle}>Perpetuity Options</Text>
+        <View style={styles.items}>
+          {Eth price }
+          {
+            optionItems.map((item, index) => {
+              return (
+                <TouchableOpacity key={index}  onPress={() => completeOption(index)}>
+                  <Option text={item} />
+                </TouchableOpacity>
+              )
+            })
+          }
+        </View>
+      </View>
+
+      </ScrollView>
+
+      {/* Write a task */}
+      {/* Uses a keyboard avoiding view which ensures the keyboard does not cover the items on screen */}
+      <KeyboardAvoidingView
+        behavior={Platform.OS === "ios" ? "padding" : "height"}
+        style={styles.writeTaskWrapper}
+      >
+        <TextInput style={styles.input} placeholder={'Write a option'} value={option} onChangeText={text => setOption(text)} />
+        <TouchableOpacity onPress={() => handleAddOption()}>
+          <View style={styles.addWrapper}>
+            <Text style={styles.addText}>+</Text>
+          </View>
+        </TouchableOpacity>
+      </KeyboardAvoidingView>
+
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#E8EAED',
+  },
+  tasksWrapper: {
+    paddingTop: 80,
+    paddingHorizontal: 20,
+  },
+  sectionTitle: {
+    fontSize: 24,
+    fontWeight: 'bold'
+  },
+  items: {
+    marginTop: 30,
+  },
+  writeTaskWrapper: {
+    position: 'absolute',
+    bottom: 60,
+    width: '100%',
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+    alignItems: 'center'
+  },
+  input: {
+    paddingVertical: 15,
+    paddingHorizontal: 15,
+    backgroundColor: '#FFF',
+    borderRadius: 60,
+    borderColor: '#C0C0C0',
+    borderWidth: 1,
+    width: 250,
+  },
+  addWrapper: {
+    width: 60,
+    height: 60,
+    backgroundColor: '#FFF',
+    borderRadius: 60,
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderColor: '#C0C0C0',
+    borderWidth: 1,
+  },
+  addText: {},
+});
